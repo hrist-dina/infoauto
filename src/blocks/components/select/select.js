88114b6ac24e8f6bc964a1d097c917bcc7641856
Dina Hrist <hdina@inbox.ru>
2019-05-25 00:56:30 +0700

    Fix article, added jquery, select2 and stylized select

---
+++ src/blocks/components/select/select.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import select2 from "select2";
+// Initialise imported function as jQuery function
+$.fn.iziModal = select2;
+
+export default class Select {
+    constructor(selector){
+        this.selector = selector;
+
+        this.init();
+    }
+
+    init() {
+        $(this.selector).select2({
+            minimumResultsForSearch: Infinity,
+            theme: 'infoauto',
+            width: 'resolve'
+        });
+    }
+
+}
---
+++ src/js/import/components.js
@@ -0,0 +1,5 @@
+import Select from "%components%/select/select";
+
+let select = new Select('.js-select');
+// eslint-disable-next-line no-console
+console.log(select);
