f01522d3701775bdefceb71c84469a9ce8acaa4e
Dina Hrist <hdina@inbox.ru>
2019-06-08 00:39:46 +0700

    Done markup modals, added base js for modal and done ask question modal

--- src/js/scripts/modal/BaseModal.js
+++ src/js/scripts/modal/BaseModal.js
@@ -4,21 +4,50 @@ import iziModal from 'izimodal';
 $.fn.iziModal = iziModal;
 
 class BaseModal {
-    constructor(selector = '.js-modal', selectorOpen = false, options = {}) {
+    constructor(
+        options = {},
+        selector = '.js-modal',
+        selectorOpen = '.js-modal-open',
+        selectorClose = '.js-modal-close'
+    ) {
         this.element = $(selector);
-        if (selectorOpen) {
-            this.elementOpen = $(selectorOpen);
-        }
-        this.options = options.length ? options : '';
+        this.elementOpen = $(selectorOpen);
+        this.selectorClose = selectorClose;
+        this.baseOptopns = {
+            radius: 0,
+            width: 'auto',
+            borderBottom: false,
+            closeButton: false
+        };
+        this.options = Object.assign(this.baseOptopns, options);
         this.init();
     }
 
     init() {
         this.element.iziModal(this.options);
+        this.onClick();
     }
 
-    open() {
-        this.element.iziModal('open');
+    onClick() {
+        const self = this;
+        this.elementOpen.on('click', function (event) {
+            event.preventDefault();
+            self.open($(this).data('modal-type'));
+        });
+        $(this.selectorClose).on('click', function (event) {
+            event.preventDefault();
+            self.close();
+        });
+    }
+
+    open(type) {
+        this.element.filter(function() {
+            return $(this).data('modal-type') === type;
+        }).iziModal('open');
+    }
+
+    close() {
+        this.element.map((item, elem) => $(elem).iziModal('close'));
     }
 }
 
